1b8b0687b492ca4de57153c550015df5b8a29500
maycon silva <maycon8609@gmail.com>
2024-01-17 16:18:37 -0300

    chore: configure prisma

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import { z } from 'zod'
 
 const envSchema = z.object({
+  DATABASE_URL: z.string(),
   NODE_ENV: z
     .enum(['development', 'production', 'test'])
     .default('development'),
