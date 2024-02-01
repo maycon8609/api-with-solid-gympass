41bfdc75917155dec46726d79fa27659e60743fc
maycon silva <maycon8609@gmail.com>
2024-02-01 15:10:58 -0300

    refactor: created factory to return instance of authenticate

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,9 +1,8 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
-import { AuthenticateUseCase } from '@/use-cases/authenticate'
 import { InvalideCredentialsError } from '@/use-cases/errors/invalide-credentials-error'
-import { PrismaUserRepository } from '@/repositories/prisma/prisma-users-repository'
+import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
 
 export async function authenticate(
   request: FastifyRequest,
@@ -17,8 +16,7 @@ export async function authenticate(
   const { email, password } = authenticateBodySchema.parse(request.body)
 
   try {
-    const prismaUserRepository = new PrismaUserRepository()
-    const authenticateUseCase = new AuthenticateUseCase(prismaUserRepository)
+    const authenticateUseCase = makeAuthenticateUseCase()
 
     await authenticateUseCase.execute({
       email,
