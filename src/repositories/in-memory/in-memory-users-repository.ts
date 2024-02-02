79a06ba83bb2f90f805c1a051142550c06cf7a47
maycon silva <maycon8609@gmail.com>
2024-02-02 00:10:50 -0300

    feat: implements findById method in user repository

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -23,4 +23,10 @@ export class InMemoryUsersRepository implements UsersRepository {
 
     return user || null
   }
+
+  async findById(id: string) {
+    const user = this.users.find((user) => user.id === id)
+
+    return user || null
+  }
 }
--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -17,4 +17,12 @@ export class PrismaUserRepository implements UsersRepository {
 
     return user
   }
+
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: { id },
+    })
+
+    return user
+  }
 }
