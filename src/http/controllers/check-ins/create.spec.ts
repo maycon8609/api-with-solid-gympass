6178ce11125ec39f1319a7b33557f9fc457f05f5
maycon silva <maycon8609@gmail.com>
2024-05-31 15:27:32 -0300

    test: add test to create check-in

---
+++ src/http/controllers/check-ins/create.spec.ts
@@ -0,0 +1,38 @@
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import { prisma } from '@/lib/prisma'
+
+describe('Check-in: Create Controller (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a check-in', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'create-mock-gym',
+        latitude: -5.060131,
+        longitude: -42.8335233,
+      },
+    })
+
+    const response = await request(app.server)
+      .post(`/gyms/${gym.id}/check-ins`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        latitude: -5.060131,
+        longitude: -42.8335233,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
