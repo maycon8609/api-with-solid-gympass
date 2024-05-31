9fd63c22a1ecfe5c6a398aa877ec326bc4bc627a
maycon silva <maycon8609@gmail.com>
2024-05-30 22:01:06 -0300

    test: add tests to nearby gym controller

---
+++ src/http/controllers/gyms/nearby.spec.ts
@@ -0,0 +1,56 @@
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Gyms: Nearby Controller (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to list nearby gyms', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'create-mock-gym-01',
+        description: 'Some description',
+        phone: '11999999999',
+        latitude: -5.060131,
+        longitude: -42.8335233,
+      })
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'create-mock-gym-02',
+        description: 'Some description',
+        phone: '11999999999',
+        latitude: -25.060131,
+        longitude: -48.8335233,
+      })
+
+    const response = await request(app.server)
+      .get('/gyms/nearby')
+      .query({
+        latitude: -5.060131,
+        longitude: -42.8335233,
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.gyms).toHaveLength(1)
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({ title: 'create-mock-gym-01' }),
+    ])
+  })
+})
