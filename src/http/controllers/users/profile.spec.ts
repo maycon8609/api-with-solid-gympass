e79dcf0c51b3dec7529c737565686b314cd1a552
maycon silva <maycon8609@gmail.com>
2024-05-30 22:17:55 -0300

    test: update profile test

--- src/http/controllers/users/profile.spec.ts
+++ src/http/controllers/users/profile.spec.ts
@@ -2,8 +2,9 @@ import request from 'supertest'
 import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
 import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
 
-describe('Profile Controller (e2e)', () => {
+describe('Users: Profile Controller (e2e)', () => {
   beforeAll(async () => {
     await app.ready()
   })
@@ -12,19 +13,8 @@ describe('Profile Controller (e2e)', () => {
     await app.close()
   })
 
-  it('should be able to get user profile', async () => {
-    await request(app.server).post('/users').send({
-      name: 'John Doe',
-      email: 'johndoe@gmail.com',
-      password: '123456',
-    })
-
-    const authResponse = await request(app.server).post('/sessions').send({
-      email: 'johndoe@gmail.com',
-      password: '123456',
-    })
-
-    const { token } = authResponse.body
+  it('should be able to get a user profile', async () => {
+    const { token } = await createAndAuthenticateUser(app)
 
     const profileResponse = await request(app.server)
       .get('/me')
