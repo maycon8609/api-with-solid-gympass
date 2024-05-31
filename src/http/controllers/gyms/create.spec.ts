41e1e7ff4c759015dffcd994bf7261ac5b73efaa
maycon silva <maycon8609@gmail.com>
2024-05-30 22:00:38 -0300

    test: add tests to create gym controller

---
+++ src/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,32 @@
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Gyms: Create Controller (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a gym', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const response = await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'create-mock-gym',
+        description: 'Some description',
+        phone: '11999999999',
+        latitude: -5.060131,
+        longitude: -42.8335233,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
