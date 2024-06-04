9185a2790b3c887b8fc30bb76ce77acd8cfcafbd
maycon silva <maycon8609@gmail.com>
2024-06-04 15:57:45 -0300

    test: update broken tests

--- src/http/controllers/gyms/nearby.spec.ts
+++ src/http/controllers/gyms/nearby.spec.ts
@@ -14,7 +14,7 @@ describe('Gyms: Nearby Controller (e2e)', () => {
   })
 
   it('should be able to list nearby gyms', async () => {
-    const { token } = await createAndAuthenticateUser(app)
+    const { token } = await createAndAuthenticateUser(app, { role: 'ADMIN' })
 
     await request(app.server)
       .post('/gyms')
--- src/http/controllers/gyms/search.spec.ts
+++ src/http/controllers/gyms/search.spec.ts
@@ -14,7 +14,7 @@ describe('Gyms: Search Controller (e2e)', () => {
   })
 
   it('should be able to search gyms by title', async () => {
-    const { token } = await createAndAuthenticateUser(app)
+    const { token } = await createAndAuthenticateUser(app, { role: 'ADMIN' })
 
     await request(app.server)
       .post('/gyms')
